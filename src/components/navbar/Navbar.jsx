0a38f6204a7f0e0a62593fb6241bc87378c3e1d9
Jefferson Balde <jeffersonbalde8@gmail.com>
2023-06-08 05:52:50 +0800

    styling navbar components

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,10 +1,25 @@
-import React from 'react'
+import './Navbar.css'
+import { SiAnaconda } from 'react-icons/si'
 
 const Navbar = () => {
   return (
-    <div>
-        <h1>Navbar</h1>
-    </div>
+    <nav className="container navbar">
+      <div className="logo">
+        <SiAnaconda color="#fff" size={33}/>
+        <p className="logo-text">
+          Social<span>X</span>
+        </p>
+      </div>
+      <menu>
+        <ul className='nav-links'>
+          <li><a href="#">Home</a></li>
+          <li><a href="#">Features</a></li>
+          <li><a href="#">Download</a></li>
+          <li><a href="#">Subscribe</a></li>
+          <li><a href="#">Get Started</a></li>
+        </ul>
+      </menu>
+    </nav>
   )
 }
 
